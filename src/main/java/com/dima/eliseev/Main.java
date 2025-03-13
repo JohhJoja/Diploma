d7400d4b5c5fb587c4606966b3eff76d088a5fd1
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-13 08:42:26 +0300

    Add interface

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -1,7 +1,15 @@
 package com.dima.eliseev;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        JFrame frame = new JFrame("Login");
+        frame.setVisible(true);
+        ImageIcon img = new ImageIcon("E:\\Java\\deeplomka\\OHPTIIFVOD\\src\\main\\java\\com\\dima\\eliseev\\img.jpg");
+        frame.setIconImage(img.getImage());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        System.out.println("U");
     }
 }
