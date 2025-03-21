6a544d67ddeeebdfd3c89432049a51483ca7a872
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-21 13:14:27 +0300

    first v thread

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -1,11 +1,15 @@
 package com.dima.eliseev;
 
 import com.dima.eliseev.auth.LoginFrame;
+import com.dima.eliseev.main_interface.Multithread;
 
 public class Main {
     public static void main(String[] args) {
 
-        new LoginFrame();
+        //new LoginFrame();
+
+        Multithread my_firstThread = new Multithread();
+        my_firstThread.start();
 
     }
 }
--- src/main/java/com/dima/eliseev/main_interface/Multithread.java
+++ src/main/java/com/dima/eliseev/main_interface/Multithread.java
@@ -1,5 +1,21 @@
 package com.dima.eliseev.main_interface;
 
-public class Multithread {
+public class Multithread extends Thread{
 
+    @Override
+    public void run() {
+        System.out.println("It's new thread!");
+        System.out.println("Do something");
+        someMethod();
+    }
+
+    static void someMethod(){
+        throw new RuntimeException();
+    }
+
+    public static void main(String[] args) {
+        new Multithread();
+    }
 }
+
+
